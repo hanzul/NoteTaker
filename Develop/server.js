5ba0cb28274242eef6c2d97a55a51434a96659f3
Hani Ghaderi <hanighaderi@Users-MacBook-Pro.local>
2020-08-11 13:37:56 -0700

    make it better

--- Develop/server.js
+++ Develop/server.js
@@ -1,8 +1,13 @@
 const express = require('express');
 const app = express();
+const path = require("path");
 const DB = require("./Develop/db/DB.js")
 const PORT = process.env.PORT || 3000
 
+// Data parsing
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+app.use(express.static("public"));
 // Routes here
 
 
