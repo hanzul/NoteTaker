4b56689de80dc40dee2521d601da3eacc3abf8a7
Hani Ghaderi <hanighaderi@Users-MacBook-Pro.local>
2020-08-11 16:30:25 -0700

    save work

--- Develop/server.js
+++ Develop/server.js
@@ -8,7 +8,11 @@ const PORT = process.env.PORT || 3000
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 app.use(express.static("public"));
-// Routes here
+// Routes
+// ===========================================================
+app.get('/', (req, res) => {
+    res.send('Welcome to the Star Wars Page!');
+  });
 
 
 //APIs
