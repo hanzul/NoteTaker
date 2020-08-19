3f4cd61153ef59d913dca488aaec2a3a289c0558
Hani Ghaderi <hanighaderi@Users-MacBook-Pro.local>
2020-08-18 20:58:09 -0700

    fixed the delete

--- Develop/db/DB.js
+++ Develop/db/DB.js
@@ -1,34 +1,36 @@
-const fs = require('fs');
-const util = require('util');
-//allow to chain promisses 
+const fs = require("fs");
+const util = require("util");
+//allow to chain promisses
 const readFAsync = util.promisify(fs.readFile);
 const writeFAsync = util.promisify(fs.writeFile);
 class DB {
-    readFile() {
-        return readFAsync("./db/db.json", "utf8")
-    }
-    // This is fs.writeFile(file,data[,options],callback)
-    writeFile(rawNote) {
+  readFile() {
+    return readFAsync("./db/db.json", "utf8");
+  }
+  // This is fs.writeFile(file,data[,options],callback)
+  writeFile(rawNote) {
+    return writeFAsync("./db/db.json", JSON.stringify(rawNote), "utf8");
+  }
 
-        return writeFAsync("./db/db.json", JSON.stringify(rawNote), "utf8")
-    }
+  readNotes() {
+    return this.readFile().then((notes) => {
+      return JSON.parse(notes);
+    });
+  }
 
-    readNotes() {
-        return this.readFile().then(notes => {
-            console.log(notes);
-            return JSON.parse(notes);
-        })
-    }
+  writeNote(newNote) {
+    console.log("NEW NOTE=>", newNote);
+    return this.writeFile(newNote).then((notes) => {
+      return this.readNotes();
+    });
+  }
 
-    writeNote(newNote) {
-        console.log("NEW NOTE=>", newNote);
-        return this.writeFile(newNote).then(notes => {
-            console.log(notes);
-            // return JSON.parse(notes);
-            return this.readNotes();
-
-        })
-    }
+  removeNote(id) {
+      console.log("remoiving note with id: ", id)
+      return this.readNotes()
+      .then(notes => notes.filter(note => note.id != id))
+      .then(fNotes => this.writeFile(fNotes))
+  }
 }
 
-module.exports = new DB()
+module.exports = new DB();
--- Develop/routes/apiRoutes.js
+++ Develop/routes/apiRoutes.js
@@ -1,38 +1,42 @@
-const router = require('express').Router()
-const DB = require('../db/DB.js')
+const router = require("express").Router();
+const DB = require("../db/DB.js");
+const uuidv1 = require('uuid/v1');
 
-router.get('/notes', async (req, res) => {
+router.get("/notes", async (req, res) => {
   try {
-    const existingNote = await DB.readNotes()
+    const existingNote = await DB.readNotes();
+    console.log(existingNote);
+    res.json(existingNote);
+  } catch (err) {
+    console.log(err);
+  }
+});
+
+router.post("/notes", async (req, res) => {
+  try {
+    const olderNotes = await DB.readNotes();
+    const note = req.body; 
+    note.id = uuidv1();
+
+    const newNote = await DB.writeNote([...olderNotes, note]);
+    res.status(200).json(newNote);
+  } catch (err) {
+    console.log(err);
+  }
+});
+
+router.delete('/notes/:id',async (req, res) => {
+  DB.removeNote(req.params.id)
+  .then(() => res.json({message: 'note deleted'}))
+  .catch(err => res.status(500).json(err))
+  /*try {
+    const delNote =
+    const existingNote = await DB.writeNote()
     console.log(existingNote);
     res.json(existingNote)
   } catch (err) {
     console.log(err);
-  }
+  }*/
 })
 
-router.post('/notes', async (req, res) => {
-  try {
-    const olderNotes=  await DB.readNotes();
-    const note = req.body;
-
-    const newNote = await DB.writeNote([...olderNotes, note]);
-    console.log(newNote);
-    res.status(200).json(newNote)
-  } catch (err) {
-    console.log(err);
-  }
-})
-
-// router.delete('/notes',async (req, res) => {
-//   try {
-//     const delNote = 
-//     const existingNote = await DB.writeNote()
-//     console.log(existingNote);
-//     res.json(existingNote)
-//   } catch (err) {
-//     console.log(err);
-//   }
-// })
-
-module.exports = router; 
+module.exports = router;
